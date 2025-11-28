61b94471251b95057a821cbfb0eaf81191c7b74e
MatheusPRodrigues <matherodrigues17@gmail.com>
2025-11-28 17:20:31 -0300

    feat: Add Matheus Branch

--- API/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c7f66dc591049f9bb0fef326da0b80d89e52bce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c555313467192b14478ba067492d8b523c7fe2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
